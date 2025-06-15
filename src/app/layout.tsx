af1a3f1a7cafe5d22fb4eefff07714aba8a13654
gabriel.estefono <gabrielestefono@gmail.com>
2025-06-15 16:14:34 -0300

    Melhorando MetaTags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,34 @@ import "../styles/global.scss";
 import { inter } from "./fonts";
 
 export const metadata: Metadata = {
-  title: "Haw Tec | Equipamentos e Serviços de Informática em Brasília",
-  description:
-    "Venda de computadores, suprimentos e manutenção com entrega gratuita em Brasília - DF. Soluções completas para empresas e usuários.",
+  title: "Haw Tec | Serviços, Equipamentos, Suprimentos e Manutenção de Informática em Brasília",
+  description: "A Haw Tec oferece equipamentos, serviços, suprimentos e manutenção de informática em Brasília. Atendimento ágil e entrega gratuita para sua empresa ou residência.",
+  alternates: {
+    languages: {
+      "pt-BR": "https://techaw.com.br",
+      "x-default": "https://techaw.com.br",
+    }
+  },
+  openGraph: {
+    title: "Haw Tec | Equipamentos e Manutenção de Informática em Brasília",
+    description: "Soluções em informática, manutenção, equipamentos e suprimentos para empresas e residências em Brasília. Atendimento ágil e entrega gratuita.",
+    type: "website",
+    url: "https://www.techaw.com.br",
+    images: [
+      {
+        url: "https://www.techaw.com.br/images/logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Haw Tec - Equipamentos e Manutenção de Informática em Brasília",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Haw Tec | Equipamentos e Manutenção de Informática em Brasília",
+    description: "Venda de equipamentos, manutenção e suprimentos de informática em Brasília. Entrega gratuita e suporte especializado.",
+    images: ["https://www.techaw.com.br/images/logo.png"],
+  },
 };
 
 export default function RootLayout({
@@ -17,6 +42,7 @@ export default function RootLayout({
     <html lang="pt-br" className={inter.variable}>
       <head>
         <JsonLdLocalBusiness />
+        <link rel="canonical" href="https://www.hawtec.com.br/" />
       </head>
       <body className="font-sans">{children}</body>
     </html>
@@ -37,7 +63,7 @@ function JsonLdLocalBusiness() {
       addressCountry: "BR",
     },
     areaServed: "Brasília",
-    logo: "https://seudominio.com.br/images/logo.png",
+    logo: "https://www.techaw.com.br/images/logo.png",
     telephone: "(61) 99999-9999",
   };
 
